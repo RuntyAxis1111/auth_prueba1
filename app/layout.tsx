da1eecfac3f12b9b68a42adbf261f6ba42b6be0e
RuntyAxis1111 <johan.martinez@hybecorp.com>
2025-07-28 15:50:51 -0600

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Montserrat } from "next/font/google"
+import { Providers } from "./providers"
+import "./globals.css"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+})
+
+export const metadata: Metadata = {
+  title: "HYBE Latin America - Data Hub",
+  description: "Artist Dashboard for HYBE Latin America",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </head>
+      <body className={montserrat.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
